01bb70e4ad70f30a7dd390f9d3f097660e13af83
Marek Fišera <mara@neptuo.com>
2022-10-04 15:44:36 +0200

    Cleanup.

--- test/PlaywrightTests.cs
+++ test/PlaywrightTests.cs
@@ -8,74 +8,55 @@ public class PlaywrightTests
     {
 #if DEBUG
         var headless = false;
+        var slowMo = 250;
 #else
         var headless = true;
+        var slowMo = 0;
 #endif
 
         var playwright = await Playwright.CreateAsync();
-        var browser = await playwright.Chromium.LaunchAsync(new() { Headless = headless });
+        var browser = await playwright.Chromium.LaunchAsync(new() { Headless = headless, SlowMo = slowMo });
         var page = await browser.NewPageAsync();
 
         await page.GotoAsync("http://127.0.0.1:9000/index.html");
-        await page.AssertText("h1", "todos");
+        Assert.Equal("todos", await page.Locator("h1").TextContentAsync());
         
         // Wait to bind events
         await Task.Delay(1500);
 
+        var todoCount = page.Locator(".todo-count");
         var todoLi = page.Locator(".todo-list li");
         var newTodo = page.Locator(".new-todo");
 
         // Check we have 0 items
-        await page.AssertText(".todo-count", "0 items left");
+        Assert.Equal("0 items left", await todoCount.TextContentAsync());
         Assert.Equal(0, await todoLi.CountAsync());
 
         // Add new item
         await newTodo.TypeAsync("Buy milk");
         Assert.Equal("Buy milk", await newTodo.InputValueAsync());
-        await DebugDelayAsync();
         await newTodo.PressAsync("Tab");
-        await DebugDelayAsync();
 
         // Check we have 1 not-completed item
         Assert.Equal(string.Empty, await newTodo.InputValueAsync());
-        await page.AssertText(".todo-count", "1 item left");
+        Assert.Equal("1 item left", await todoCount.TextContentAsync());
         Assert.Equal(1, await todoLi.CountAsync());
-        await page.AssertText(todoLi.First.Locator("label"), "Buy milk");
+        Assert.Equal( "Buy milk", await todoLi.First.Locator("label").TextContentAsync());
         Assert.DoesNotContain("completed", await todoLi.First.GetAttributeAsync("class"));
 
         // Complete item
         await todoLi.First.Locator("input[type=checkbox]").ClickAsync();
-        await DebugDelayAsync();
 
         // Check we have 0 not-completed items
-        await page.AssertText(".todo-count", "0 items left");
+        Assert.Equal("0 items left", await todoCount.TextContentAsync());
         Assert.Equal(1, await todoLi.CountAsync());
         Assert.Contains("completed", await todoLi.First.GetAttributeAsync("class"));
 
         // Delete item
         await todoLi.First.Locator(".destroy").ClickAsync();
-        await DebugDelayAsync();
 
         // Check we have 0 items
-        await page.AssertText(".todo-count", "0 items left");
+        Assert.Equal("0 items left", await todoCount.TextContentAsync());
         Assert.Equal(0, await todoLi.CountAsync());
-
-        await DebugDelayAsync(5 * 1000);
     }
-
-    private async Task DebugDelayAsync(int millisecondsDelay = 500)
-    {
-#if DEBUG
-        await Task.Delay(millisecondsDelay);
-#endif
-    }
-}
-
-public static class AssertExtensions
-{
-    public static Task AssertText(this IPage page, string locator, string text) 
-        => AssertText(page, page.Locator(locator), text);
-
-    public static async Task AssertText(this IPage page, ILocator locator, string text) 
-        => Assert.Equal(text, await locator.TextContentAsync());
 }
