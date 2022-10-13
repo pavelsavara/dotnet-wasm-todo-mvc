6032c7bf0a47cbe8a7748ca1f6c57adff4acf14f
Michael Bond <michael@bondcodes.com>
2022-10-13 16:48:13 -0400

    Fix JS

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 import { dotnet } from "./dotnet.js"
 import { $on } from "./helpers.js";
 
-let exports;
-const onHashchange = () => exports.TodoMVC.MainJS.OnHashchange(document.location.hash);
-$on(window, "hashchange", onHashchange);
-
 const { getAssemblyExports, getConfig } = await dotnet.create();
 
-exports = await getAssemblyExports(getConfig().mainAssemblyName);
+let exports = await getAssemblyExports(getConfig().mainAssemblyName);
+
+debugger;
+
+const onHashchange = () => exports.TodoMVC.MainJs.OnHashchange(document.location.hash);
+$on(window, "hashchange", onHashchange);
 
 await dotnet.run();
 onHashchange();
